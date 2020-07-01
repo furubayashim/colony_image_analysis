69a091d4146389aa700eb981669f87fd146618f4
mfurubayashi <maiko.furubayashi@gmail.com>
2020-07-01 12:44:16 +0900

    planning new functions to make

--- colony.py
+++ colony.py
@@ -33,6 +33,16 @@ def averageRGB(image):
     # get average
     return np.round(npim.mean(axis=0),2)
 
+def get_round_colony_area():
+    # select colony items with high roundness
+    # return area value of that item in a array? avg? stdev?
+    return None
+
+def remove_conjoined_colonies():
+    # find items that have relatively low roundness but relatively round get_round_colony_area
+    # and remove thme from df
+    return None
+
 df['CropIm'] = df.apply(lambda x: cropColony(x,im),axis=1)
 df['CropImSize'] = [x.size[0]*x.size[1] for x in df['CropIm']]
 df['average_RGB'] = [averageRGB(x) for x in df['CropIm']]
